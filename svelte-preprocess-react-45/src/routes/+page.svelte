03cc95085378b61e9400fb2e10b0a070e2a48f8d
Reijhanniel Jearl Campos <devcsrj@gmail.com>
2024-11-06 13:07:16 +0800

    chore: Setup tldraw

--- svelte-preprocess-react-45/src/routes/+page.svelte
+++ svelte-preprocess-react-45/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import {Tldraw, useEditor} from 'tldraw';
+  import 'tldraw/tldraw.css';
+  import {hooks} from "svelte-preprocess-react";
+
+  // const editor = hooks(() => useEditor());
+  // console.log('editor', editor)
+</script>
+
+<main>
+  <react:Tldraw
+      options={{
+				maxPages: 1
+			}}
+  />
+</main>
+
+<style>
+  main {
+      padding: 2rem;
+      height: calc(100vh - 10rem);
+      width: calc(100vw - 10rem);
+      box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+</style>
--- svelte-preprocess-react-45/svelte.config.js
+++ svelte-preprocess-react-45/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {sequence} from "@sveltejs/kit/hooks";
+import preprocessReact from "svelte-preprocess-react/preprocessReact";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), preprocessReact()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
